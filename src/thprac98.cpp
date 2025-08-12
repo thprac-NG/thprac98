60ffa14a4e2704d9c9f09fce0a036c64f47d015f
H-J-Granger <hermione_granger@foxmail.com>
2025-08-12 20:24:43 +0800

    refactor: Refactor the main loop implementation

--- src/thprac98.cpp
+++ src/thprac98.cpp
@@ -5,17 +5,24 @@
 #include "src/utils.hpp"
 #include "src/menu.hpp"
 
+volatile bool main_loop_end = false;
+const int MAIN_LOOP_CYCLE = 2;   // unit: *10ms
+void main_loop() {
+  // Reduce the usage of key_scan.
+  if (is_pressed(KEY_Q_TA)) {
+    main_loop_end = true;
+    return;
+  }
+}
+
 int main() {
   // text_putca(3, 3, 0x8c, TX_CYAN);
   // return 0;
 
   launcher_menu_init();
-  while (1) {  // main loop
-    if (is_pressed(KEY_Q_TA)) {
-      puts("Key Q pressed. Exiting.");
-      break;
-    }
-  }
+  timer_start(MAIN_LOOP_CYCLE, main_loop);
+  while (main_loop_end == false) {}
+  timer_end();
   launcher_menu_clear();
   return 0;
 }
