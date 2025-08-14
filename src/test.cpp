db7f830bba00bf9ae9a4ac048dd822fc50a6d1af
H-J-Granger <hermione_granger@foxmail.com>
2025-08-14 23:02:35 +0800

    fix: Fix some warning from static expressions in `if`s

--- src/test.cpp
+++ src/test.cpp
@@ -5,20 +5,13 @@
 #include "src/utils.hpp"
 
 bool test_stdint(void) {
-#define test_stdint_macro(bit_length)                           \
-  if (!((sizeof(uint##bit_length) * 8 == bit_length) &&         \
-        ((uint##bit_length)(-1) > (uint##bit_length)(0)))) {    \
-    return false;                                               \
-  }                                                             \
-  if (!((sizeof(int##bit_length) * 8 == bit_length) &&          \
-        ((int##bit_length)(-1) < (int##bit_length)(0)))) {      \
-    return false;                                               \
-  }
+#define test_stdint_macro(bit_length)                    \
+    (sizeof(uint##bit_length) * 8 == bit_length) &&      \
+    ((uint##bit_length)(-1) > (uint##bit_length)(0)) &&  \
+    (sizeof(int##bit_length) * 8 == bit_length) &&       \
+    ((int##bit_length)(-1) < (int##bit_length)(0))
 
-  test_stdint_macro(8);
-  test_stdint_macro(16);
-  test_stdint_macro(32);
-  return true;
+  return test_stdint_macro(8) && test_stdint_macro(16) && test_stdint_macro(32);
 #undef test_stdint_macro
 }
 
