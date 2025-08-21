3890a39f68f1eae856d4ce15a41be001ba89a15f
H-J-Granger <hermione_granger@foxmail.com>
2025-08-21 22:40:01 +0800

    feat: add a help menu text

--- src/textsdef.cpp
+++ src/textsdef.cpp
@@ -4,12 +4,13 @@
 
 #include "src/textsdef.hpp"
 
-const char* th_glossary_str[3][11] = {
+const char* th_glossary_str[3][12] = {
   {
     " —V‹Y ",
     " èYÚ ",
     " H‹ï ",
     " İ’u ",
+    " ›ñ• ",
     "­—‹F“˜’†cc",
     "thprac98 - “Œ•û‹Œì—V‹YŒ[“®Ší",
     "“Œ•ûèËˆÙ“`@` Highly Responsive to Prayers.",
@@ -23,6 +24,7 @@ const char* th_glossary_str[3][11] = {
     " Link ",
     " Tool ",
     "Config",
+    " Help ",
     "Girls are now praying...",
     "thprac98 - PC-98 Touhou Game Launcher",
     "Touhou Reiiden ~ Highly Responsive to Prayers.",
@@ -36,6 +38,7 @@ const char* th_glossary_str[3][11] = {
     " Øİ¸  ",
     " Â°Ù  ",
     " İ’è ",
+    " ÍÙÌß ",
     "­—‹F“˜’†cc",
     "thprac98 - “Œ•û‹ŒìƒQ[ƒ€ƒ‰ƒ“ƒ`ƒƒ[",
     "“Œ•ûèËˆÙ“`@` Highly Responsive to Prayers.",
@@ -46,11 +49,12 @@ const char* th_glossary_str[3][11] = {
   }
 };
 
-const th_glossary_t THPRAC_FX_MENUS[4] = {
+const th_glossary_t THPRAC_FX_MENUS[5] = {
   THPRAC_F1_GAME,
   THPRAC_F2_LINK,
   THPRAC_F3_TOOLS,
-  THPRAC_F4_CONFIG
+  THPRAC_F4_CONFIG,
+  THPRAC_F5_HELP
 };
 
 const th_glossary_t THPRAC_GAME_TITLES[5] = {
--- src/textsdef.hpp
+++ src/textsdef.hpp
@@ -10,6 +10,7 @@ enum th_glossary_t {
   THPRAC_F2_LINK,
   THPRAC_F3_TOOLS,
   THPRAC_F4_CONFIG,
+  THPRAC_F5_HELP,
   THPRAC_GIRLS_ARE_PRAYING,
   THPRAC_LAUNCHER_TITLE,
   THPRAC_TH01_TITLE,
@@ -19,9 +20,9 @@ enum th_glossary_t {
   THPRAC_TH05_TITLE
 };
 
-extern const char* th_glossary_str[3][11];
+extern const char* th_glossary_str[3][12];
 
-extern const th_glossary_t THPRAC_FX_MENUS[4];
+extern const th_glossary_t THPRAC_FX_MENUS[5];
 extern const th_glossary_t THPRAC_GAME_TITLES[5];
 
 #endif  // #ifndef THPRAC98_SRC_TEXTSDEF_HPP_
