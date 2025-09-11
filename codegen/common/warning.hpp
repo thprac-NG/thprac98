74412bfc4c51c0b587c5ecef8563f8637c664394
H-J-Granger <hermione_granger@foxmail.com>
2025-09-11 11:51:49 +0800

    refactor: Move the code-gen warning to a common/ folder

---
+++ codegen/common/warning.hpp
@@ -0,0 +1,12 @@
+#ifndef THPRAC98_CODEGEN_COMMON_WARNING_HPP_
+#define THPRAC98_CODEGEN_COMMON_WARNING_HPP_
+
+#define ENDL "\n"
+#define INDENT "  "
+
+#define THPRAC98_CODEGEN_WARNING_TEXT \
+    "// WARNING: THIS FILE IS AUTO-GENERATED" ENDL                  \
+    "// To modify this file, edit codegen/texts.json and use" ENDL  \
+    "// codegen/texts_gen.cpp to regenerate this file." ENDL ENDL
+
+#endif  // #ifndef THPRAC98_CODEGEN_COMMON_WARNING_HPP_
