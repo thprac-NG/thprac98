fe49e26a2a118634730a42404a7211839f28e850
H-J-Granger <hermione_granger@foxmail.com>
2026-04-28 04:57:48 +0800

    chore: Remove tsrdata.* from the repo

--- src/tsrdata.hpp
+++
@@ -1,6 +0,0 @@
-// WARNING: THIS FILE IS AUTO-GENERATED
-// To modify this file, rebuild the TH0X.COM files and 
-// use codegen/comembed.cpp to regenerate this file.
-// clang-format off
-
-extern unsigned char th01_com_data[3400];
